6c49e8b278db465b914636243dd6d8f49d88bcf0
Roy Elaawar <roy.elaawar@gmail.com>
2023-09-20 14:09:57 -0400

    creating and inserting DOM

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const element = document.getElementById("main")
+element.remove()
+const newHeader = document.createElement("h1");
+newHeader.id= "victory"
+newHeader.innerHTML = "Roy is the champion"
